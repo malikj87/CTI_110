9b4366d902bc8126754248c840d563f156acd365
malikj87 <150169181+malikj87@users.noreply.github.com>
2023-11-09 11:51:30 -0500

    Add files via upload

---
+++ P1HW2_JoshuaDavid.py
@@ -0,0 +1,57 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/malik/Downloads/P1HW2_JoshuaDavid.py
+The program calulates and displays travel expenses
+Enter Budget:500
+Enter your travel destination:Texas
+How much do you think you will spend on gas?:50
+Approximately, how much will you need for accomodation/hotel?250
+How much do you need for food?:40
+
+============ RESTART: C:/Users/malik/Downloads/P1HW2_JoshuaDavid.py ============
+The program calulates and displays travel expenses
+Enter Budget:500
+Enter your travel destination:Texas
+How much do you think you will spend on gas?:50
+Approximately, how much will you need for accomodation/hotel?150
+How much do you need for food?:50
+-------Travel Expenses--------
+Location: Texas
+Initial Budget: 500
+Fuel: 50
+Accomodation: 150
+Food: 50
+>>> 
+============ RESTART: C:/Users/malik/Downloads/P1HW2_JoshuaDavid.py ============
+The program calulates and displays travel expenses
+Enter Budget:500
+Enter your travel destination:Texas
+How much do you think you will spend on gas?:50
+Approximately, how much will you need for accomodation/hotel?150
+How much do you need for food?:50
+-------Travel Expenses--------
+Location: Texas
+Initial Budget: 500
+Fuel: 50
+Accomodation: 150
+Food: 50
+Traceback (most recent call last):
+  File "C:/Users/malik/Downloads/P1HW2_JoshuaDavid.py", line 18, in <module>
+    print("Remaining Balance:", Budget-Travel_Dest-Gas-Accomodation-Food)
+TypeError: unsupported operand type(s) for -: 'int' and 'str'
+>>> 
+============ RESTART: C:/Users/malik/Downloads/P1HW2_JoshuaDavid.py ============
+The program calulates and displays travel expenses
+Enter Budget:500
+Enter your travel destination:Texas
+How much do you think you will spend on gas?:150
+Approximately, how much will you need for accomodation/hotel?100
+How much do you need for food?:50
+-------Travel Expenses--------
+Location: Texas
+Initial Budget: 500
+Fuel: 150
+Accomodation: 100
+Food: 50
+Remaining Balance: 200
